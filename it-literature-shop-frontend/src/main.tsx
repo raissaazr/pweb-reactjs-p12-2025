7337373b11049efc694354de37d122fafafc968a
Khairul Yahya <khairulyahya417@gmail.com>
2025-11-08 00:45:36 +0700

    Wrap App component with CartProvider

--- it-literature-shop-frontend/src/main.tsx
+++ it-literature-shop-frontend/src/main.tsx
@@ -1,14 +1,18 @@
-// src/main.tsx
+// src/main.tsx (Perbaikan)
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css' // Asumsi Anda punya file CSS global
-import { AuthProvider } from './contexts/authcontext'; // <-- IMPORT
+import './index.css'
+import { AuthProvider } from './contexts/AuthContext';
+import { CartProvider } from './contexts/cartcontext'; // <-- 1. IMPORT CartProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider> {/* <-- BUNGKUS DI SINI */}
-      <App />
-    </AuthProvider> {/* <-- BUNGKUS DI SINI */}
+    <AuthProvider>
+      <CartProvider> {/* <-- 2. BUNGKUS DI SINI */}
+        <App />
+      </CartProvider> {/* <-- 3. BUNGKUS DI SINI */}
+    </AuthProvider>
   </React.StrictMode>,
 )
