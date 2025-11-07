e2ce4168eaac3d62648da5b9314ee137b6346f12
Khairul Yahya <khairulyahya417@gmail.com>
2025-11-08 00:46:59 +0700

    Fix import path for AuthContext in ProtectedRoute

--- it-literature-shop-frontend/src/components/protectedroute.tsx
+++ it-literature-shop-frontend/src/components/protectedroute.tsx
@@ -1,7 +1,7 @@
 // src/components/ProtectedRoute.tsx
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../contexts/authcontext';
+import { useAuth } from '../contexts/AuthContext';
 
 const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
