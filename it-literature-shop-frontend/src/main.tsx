4be8eeb44b2d7563109289721bdd9dff6f160dcf
Khairul Yahya <khairulyahya417@gmail.com>
2025-11-08 00:52:52 +0700

    Update main.tsx

--- it-literature-shop-frontend/src/main.tsx
+++ it-literature-shop-frontend/src/main.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from './contexts/authcontext';
 import { CartProvider } from './contexts/cartcontext'; // <-- 1. IMPORT CartProvider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
